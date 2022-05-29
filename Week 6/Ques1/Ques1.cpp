4a93f3c2962d3a0c4ff22467f4910ab21f6a3240
isha0612 <isha06dec@gmail.com>
2022-05-30 00:13:31 +0530

    week6 ques1

--- Week 6/Ques1/Ques1.cpp
+++ Week 6/Ques1/Ques1.cpp
@@ -26,7 +26,7 @@ int main() {
         graph[v1].push_back(v2);
         graph[v2].push_back(v1);
     }
-    for(int i = 1; i <= 5;i ++) {
+    for(int i = 1; i <= 5; i++) {
         cout<<i<<"->";
         for(auto x : graph[i]) {
             cout<<x<<" ";
